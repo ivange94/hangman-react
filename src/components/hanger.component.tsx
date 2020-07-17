21678787ce22e985b6705258183cdb47e1da31af
Ivange  Larry <ivangelarry@gmail.com>
2020-07-17 10:57:07 +0100

    Building hanger

--- src/components/hanger.component.tsx
+++ src/components/hanger.component.tsx
@@ -4,6 +4,10 @@ import styles from './hanger.module.css';
 export default function Hanger() {
     return (
         <div className={styles.wrapper}>
+            <div className={styles.verticalStickBottom}></div>
+            <div className={styles.verticalStick}></div>
+            <div className={styles.horizontalStick}></div>
+            <div className={styles.rope}></div>
             <div className={styles.head}></div>
             <div className={styles.torso}></div>
             <div className={styles.leftarm}></div>
